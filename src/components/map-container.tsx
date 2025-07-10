885c7b2c24e0ce2670cf417b613ccdf47b02ff4c
Chaesunbak <coanstjd0422@gmail.com>
2025-07-11 02:39:31 +0900

    fix : 맵 컴포넌트 메모이제이션 적용

--- src/components/map-container.tsx
+++ src/components/map-container.tsx
@@ -1,55 +1,52 @@
 import { Container as MapDiv, NaverMap, useNavermaps } from "react-naver-maps";
-import { useState, useMemo, memo } from "react";
-import debounce from "lodash/debounce";
+import { useState, memo } from "react";
+import { useDebounceCallback } from "usehooks-ts";
 
 import type { Coordinates, Place } from "@/types";
 import { MyLocationControl } from "@/components/my-location-control";
 import { MarkerClusterer } from "@/components/marker-clusterer";
 import { CategoryFilters } from "@/components/category-filters";
-import { useMapContext } from "@/hooks/use-map-context";
+import { DEFAULT_LOCATION } from "@/lib/constants";
 
-export const MapContainer = memo(function MapContainer({
+function PureMapContainer({
   places,
   filters,
   setFilters,
   setSearchInput,
   setMyLocation,
+  setMap,
+  moveTo,
 }: {
   places: Place[];
   filters: string[];
   setFilters: (filters: string[]) => void;
   setSearchInput: (searchInput: string) => void;
   setMyLocation: (location: Coordinates | null) => void;
+  setMap: (map: naver.maps.Map | null) => void;
+  moveTo: (coords: Coordinates, zoom?: number) => void;
 }) {
   const navermaps = useNavermaps();
-  const { setMap } = useMapContext();
   const [zoom, setZoom] = useState<number>(15);
   const [bounds, setBounds] = useState<naver.maps.PointBounds | null>(null);
 
-  const debouncedSetBounds = useMemo(
-    () =>
-      debounce((bounds: naver.maps.PointBounds) => {
-        setBounds(bounds);
-      }, 200),
-    [],
+  const debouncedSetBounds = useDebounceCallback(
+    (bounds: naver.maps.PointBounds) => {
+      setBounds(bounds);
+    },
+    200,
   );
-
-  const defaultCenter: Coordinates = {
-    lat: 37.3595704,
-    lng: 127.105399,
-  }; // 네이버 그린팩토리
 
   return (
     // 19.5rem = SIDEBAR_WIDTH
     <MapDiv className="h-full w-full overflow-hidden lg:w-[calc(100%-19.5rem)]">
       <div className="absolute top-0 left-0 z-20 mt-2 flex w-full justify-between gap-2 px-0 md:px-2">
-        <MyLocationControl setMyLocation={setMyLocation} />
+        <MyLocationControl setMyLocation={setMyLocation} moveTo={moveTo} />
         {/* 필터 버튼 */}
         <CategoryFilters filters={filters} setFilters={setFilters} />
       </div>
       <NaverMap
         defaultCenter={
-          new navermaps.LatLng(defaultCenter.lat, defaultCenter.lng)
+          new navermaps.LatLng(DEFAULT_LOCATION.lat, DEFAULT_LOCATION.lng)
         }
         defaultZoom={15}
         ref={setMap}
@@ -66,8 +63,11 @@ export const MapContainer = memo(function MapContainer({
           zoom={zoom}
           bounds={bounds}
           setSearchInput={setSearchInput}
+          moveTo={moveTo}
         />
       </NaverMap>
     </MapDiv>
   );
-});
+}
+
+export const MapContainer = memo(PureMapContainer);
